82f81239c8726d2c7880c439bb4969c769e442c9
Weiran <huangweiran1998@gmail.com>
2020-01-20 16:07:59 +0800

    add some basic testing

--- warm-up/testing.py
+++ warm-up/testing.py
@@ -5,6 +5,34 @@ I don't really want to treat it too serious. :)
 
 """
 
+# test cross entropy loss
 from Loss import CrossEntropyLoss
 import torch
+
+X = torch.randn(6, 8, requires_grad=True)
+y = torch.tensor([3,4,3,5,1,0])
+
+loss1 = torch.nn.CrossEntropyLoss()
+l1 = loss1(X, y)
+print(l1)
+
+loss2 = CrossEntropyLoss()
+l2 = loss2.forward(X.detach().numpy(), y.numpy())
+print(l2)
+
+l1.backward()
+print(X.grad)
+
+grads = loss2.backward()
+print(grads)
+
+# test sigmoid, only the forward pass.
+from Activations import Sigmoid
+
+X = torch.randn(4,5)
+sigmoid = torch.nn.Sigmoid()
+print(sigmoid(X))
+print(Sigmoid.forward(X.numpy())[0])
+
+
 
