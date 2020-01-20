f5084a39c08f956fade10effc7d3bc14acb8207c
Weiran <huangweiran1998@gmail.com>
2020-01-20 15:53:32 +0800

    change LOSS class input

--- warm-up/Loss.py
+++ warm-up/Loss.py
@@ -8,18 +8,21 @@ from typing import Tuple
 
 
 class Loss:
-    @staticmethod
-    def forward(input: np.ndarray, target: np.ndarray) -> Tuple[float, Tuple[np.ndarray, np.ndarray]]:
+    def __init__(self):
+        self.cache = None
+
+    def forward(self, inputs: np.ndarray, target: np.ndarray) -> Tuple[float, Tuple[np.ndarray, np.ndarray]]:
         raise NotImplementedError
 
-    @staticmethod
-    def backward(dout, cache):
+    def backward(self, dout):
         raise NotImplementedError
 
 # TODO: add type and shape checking
 class CrossEntropyLoss(Loss):
-    @staticmethod
-    def forward(input: np.ndarray, target: np.ndarray) -> Tuple[float, Tuple[np.ndarray, np.ndarray]]:
+    def __init__(self):
+        super(CrossEntropyLoss, self).__init__()
+
+    def forward(self, inputs: np.ndarray, target: np.ndarray) -> float:
         """
 
         :param input: N x D ndarray, each row as a vector.
@@ -29,27 +32,27 @@ class CrossEntropyLoss(Loss):
         a class index, i.e., integer ranging from 0 to D-1 (included).
         :return: a scalar, the loss value.
         """
-        if len(input.shape) == 1:
-            input = input.reshape(1, -1)
+        if len(inputs.shape) == 1:
+            inputs = inputs.reshape(1, -1)
         # if len(target.shape) == 1:
         #     target = target.reshape(1, -1)
 
         # class_idx = np.argmax(target, axis=1)
 
         # scale the input
-        input_max = np.max(input, axis=1, keepdims=True)  # N x 1
-        input -= input_max
+        input_max = np.max(inputs, axis=1, keepdims=True)  # N x 1
+        inputs -= input_max
 
-        input_exp = np.exp(input)
-        loss = -input[np.arange(len(input)), target][:, None] + np.log(np.sum(input_exp, axis=1, keepdims=True))
+        input_exp = np.exp(inputs)
+        loss = -inputs[np.arange(len(inputs)), target][:, None] + np.log(np.sum(input_exp, axis=1, keepdims=True))
+        loss = np.mean(loss).squeeze()
 
-        cache = input_exp, target
+        self.cache = input_exp, target
 
-        return loss, cache
+        return loss
 
-    @staticmethod
-    def backward(cache, dout=1):
-        input_exp, target = cache
+    def backward(self, dout=1):
+        input_exp, target = self.cache
         grads = input_exp / np.sum(input_exp, axis=1, keepdims=True)
 
         grads[np.arange(len(grads)), target] -= 1
@@ -66,13 +69,12 @@ if __name__ == '__main__':
         [-0.2652, -0.3018, -0.2923, -0.5061,  1.3517]])
     y = np.array([3,4,3])
 
-    l, cache = loss.forward(x, y)
-    grads = loss.backward(cache)
-    print(loss)
+    l = loss.forward(x, y)
+    grads = loss.backward()
+    print(l)
     print(grads)
 
 # [[ 0.0374,  0.2280,  0.0232, -0.3181,  0.0295],
 #  [ 0.0342,  0.0303,  0.1325,  0.0781, -0.2752],
 #  [ 0.0381,  0.0367,  0.0370, -0.3034,  0.1917]]
 
-    print(np.mean(loss.forward(x, y)[0]))
