0f9ca8b06fd7fdd6cf8eda6de724c7c3940d5e1c
Weiran <huangweiran1998@gmail.com>
2020-01-26 19:04:22 +0800

    finished dense layer bp && Tested all

--- warm-up/Layers.py
+++ warm-up/Layers.py
@@ -14,17 +14,18 @@ class Module:
     def forward(self, inputs):
         raise NotImplementedError
 
-    def backward(self):
+    def backward(self, dout):
         raise NotImplementedError
 
 
 class Dense(Module):
-    def __init__(self, input_dim: int, output_dim: int, use_bias: bool, activation: str) -> None:
+    def __init__(self, input_dim: int, output_dim: int, use_bias: bool = False, activation: str = "relu") -> None:
         super(Dense, self).__init__(input_dim, output_dim)
 
         self.use_bias = use_bias
         self.weight = None
         self.bias = None
+        self.cache = {}
 
         if activation == "sigmoid":
             self.activation = Sigmoid
@@ -41,19 +42,50 @@ class Dense(Module):
             self.bias = zero_initializer(1, self.output_dim)
 
     def forward(self, inputs):
+        """
+
+        :param inputs: fed data, m x input_dim, m is the batch size.
+        :return:
+        """
         # TODO: deal with the cache part
         if self.use_bias:
             affined = np.dot(inputs, self.weight) + self.bias
+            self.cache['W'] = self.weight
+            self.cache['b'] = self.bias
+            self.cache['x'] = inputs
         else:
             affined = np.dot(inputs, self.weight)
+            self.cache['W'] = self.weight
+            self.cache['x'] = inputs
 
         act = self.activation.forward
-        output, cache = act(affined)
+        output, act_cache = act(affined)
+        self.cache['act'] = act_cache
 
-        return output, cache
+        return output
 
-    def backward(self):
-        raise NotImplementedError
+    def backward(self, dout):
+        """
+        :param dout: The gradient of the output. shape: m x output_dim, m is the batch size.
+        :return:
+        """
+
+        # backprop of activation
+        act_back = self.activation.backward
+        d_linear = act_back(dout, self.cache['act'])
+
+        # backprop of linear layer
+        dW = self.cache['x'].T @ d_linear
+        dx = d_linear @ (self.cache['W'].T)
+        if self.bias is not None:
+            db = np.sum(d_linear, axis=0, keepdims=True)
+
+            grads = {'dW': dW, 'dx': dx, 'db': db}
+        else:
+            grads = {'dW': dW, 'dx': dx}
+
+        return grads
+
 
 
 class Dropout(Module):
@@ -67,5 +99,19 @@ class Dropout(Module):
     def forward(self, inputs):
         pass
 
-    def backward(self):
+    def backward(self, dout):
         pass
+
+
+if __name__ == '__main__':
+    dense = Dense(10, 6, use_bias=True, activation="sigmoid")
+    x = np.random.randn(4, 10)
+
+    dense.init_parameters()
+    print('---------------Parameters----------------')
+    print(dense.weight, '\n', dense.bias)
+    y = dense.forward(x)
+    print(y)
+
+    dout = np.ones((4, 6))
+    print(dense.backward(dout))
--- warm-up/testing.py
+++ warm-up/testing.py
@@ -8,6 +8,7 @@ I don't really want to treat it too serious. :)
 # test cross entropy loss
 from Loss import CrossEntropyLoss
 import torch
+import numpy as np
 
 X = torch.randn(6, 8, requires_grad=True)
 y = torch.tensor([3,4,3,5,1,0])
@@ -26,13 +27,77 @@ print(X.grad)
 grads = loss2.backward()
 print(grads)
 
-# test sigmoid, only the forward pass.
+# test sigmoid's forward and backward pass.
 from Activations import Sigmoid
 
-X = torch.randn(4,5)
+X = torch.randn(4,5, requires_grad=True)
 sigmoid = torch.nn.Sigmoid()
-print(sigmoid(X))
-print(Sigmoid.forward(X.numpy())[0])
+y = sigmoid(X)
+print(y)
+
+y_self, cache_self = Sigmoid.forward(X.detach().numpy())
+#print(Sigmoid.forward(X.detach().numpy())[0])
+
+y.sum().backward()
+print(X.grad)
+
+grads = Sigmoid.backward(1, cache_self)
+print(grads)
+
+
+# test relu's forward and backward pass.
+from Activations import Relu
+relu = torch.nn.ReLU()
+
+X = torch.randn(7,8, requires_grad=True)
+y = relu(X)
+print(y)
+
+y_self, cache_self = Relu.forward(X.detach().numpy())
+print(y_self)
+
+y.sum().backward()
+print(X.grad)
+
+grads = Relu.backward(np.ones_like(X.detach().numpy()), cache_self)
+print(grads)
+
+
+# test the dense layer
+from Layers import Dense
+
+dense = Dense(10, 6, use_bias=True, activation="sigmoid")
+x = np.random.randn(4, 10)
+
+dense.init_parameters()
+print('---------------Parameters----------------')
+print(dense.weight, '\n', dense.bias)
+y = dense.forward(x)
+print(y)
+
+
+from torch.nn.functional import linear
+weight, bias = torch.tensor(dense.weight, requires_grad=True), torch.tensor(dense.bias, requires_grad=True)
+inputs = torch.tensor(x, requires_grad=True)
+
+t_output = linear(inputs, weight.T, bias)
+output = sigmoid(t_output)
+print(output)
+
+dout = np.ones((4, 6))
+grads = dense.backward(dout)
+
+output.sum().backward()
+print(grads['dx'])
+print(inputs.grad)
+
+print(grads['dW'])
+print(weight.grad)
+
+print(grads['db'])
+print(bias.grad)
+
+
 
 
 
